9b43c1defd0fca7e0620322c1a2e97de2c466b67
Eric L Demate <eric.demate@gmail.com>
2022-08-05 13:15:19 -0300

    finalização projeto

--- TTD Calculadora/Mocha_Chai/src/Calculadora.js
+++ TTD Calculadora/Mocha_Chai/src/Calculadora.js
@@ -14,24 +14,24 @@ export default class Calculadora {
         var total = (a * b)
         return parseFloat(total.toFixed(2))
     }
-    
+
     static div(a, b) { //realiza operações de divisão
         var total = (a / b)
         return parseFloat(total.toFixed(2))
     }
 
     static expow(a, b) { //realiza operações exponenciais
-        var total = Math.pow(a , b)
-        return parseFloat(total.toFixed(2)) 
+        var total = Math.pow(a, b)
+        return parseFloat(total.toFixed(2))
     }
-    
+
     static percentil(a, b) { //realiza operações de medidas divididas em até 100 partes
-        var total = ((a/100)*b)
+        var total = ((a / 100) * b)
         return parseFloat(total.toFixed(2))
     }
 
     static permil(a, b) { //realiza operações de medidas divididas em até 1000 partes
-        var total = ((a/1000)*b)
+        var total = ((a / 1000) * b)
         return parseFloat(total.toFixed(2))
     }
     static raiz(a) { //realiza operações raiz quadrada de um número
